4d735554eb53f74508171d606eead4bbb18f4d25
1733856225@qq.com <1733856225@qq.com>
2019-07-23 23:29:55 +0800

    restTemplate的编写

---
+++ SpringCloudBorn-provider-dept-8001/src/main/java/org/SpringCloudBorn/provider/App.java
@@ -0,0 +1,19 @@
+package org.SpringCloudBorn.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+//@EnableEurekaClient
+//@EnableDiscoveryClient
+public class App {
+    public static void main(String[] args){
+    	
+		SpringApplication.run(App.class, args);
+		
+	}
+}
---
+++ SpringCloudBorn-provider-dept-8001/src/main/java/org/SpringCloudBorn/provider/service/impl/DeptServiceImpl.java
@@ -0,0 +1,35 @@
+package org.SpringCloudBorn.provider.service.impl;
+
+import java.util.List;
+
+import org.SpringCloudBorn.provider.dao.DeptDao;
+import org.SpringCloudBorn.provider.service.DeptService;
+import org.SpringCloudBorn_api.entities.Dept;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DeptServiceImpl implements DeptService
+{
+	@Autowired
+	private DeptDao dao;
+	
+	@Override
+	public boolean add(Dept dept)
+	{
+		return dao.addDept(dept);
+	}
+
+	@Override
+	public Dept get(Long id)
+	{
+		return dao.findById(id);
+	}
+
+	@Override
+	public List<Dept> list()
+	{
+		return dao.findAll();
+	}
+
+}
