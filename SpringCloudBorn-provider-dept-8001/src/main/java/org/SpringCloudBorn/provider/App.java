ff1bc12cfb5ce5e6a32835c5a2ca6658ace943eb
1733856225@qq.com <1733856225@qq.com>
2019-07-23 23:56:15 +0800

    ip修改

--- SpringCloudBorn-provider-dept-8001/src/main/java/org/SpringCloudBorn/provider/App.java
+++ SpringCloudBorn-provider-dept-8001/src/main/java/org/SpringCloudBorn/provider/App.java
@@ -2,13 +2,14 @@ package org.SpringCloudBorn.provider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Hello world!
  *
  */
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 //@EnableDiscoveryClient
 public class App {
     public static void main(String[] args){
