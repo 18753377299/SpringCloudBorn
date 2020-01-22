3762857c6eb743d20dbe092cae4a3c91c912def6
qk l <1733856225@qq.com>
2020-01-22 10:48:28 +0800

    整合文件

--- SpringCloudBorn-consumer-dept-80/src/main/java/com/example/controller/DeptController_Consumer.java
+++ SpringCloudBorn-consumer-dept-80/src/main/java/com/example/controller/DeptController_Consumer.java
@@ -3,7 +3,12 @@ package com.example.controller;
 import java.util.List;
 
 import org.SpringCloudBorn_api.entities.Dept;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +21,7 @@ public class DeptController_Consumer
 //	private static final String REST_URL_PREFIX = "http://localhost:8001";
 	private static final String REST_URL_PREFIX = "http://SPRINGCLOUDBORN-DEPT";
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(DeptController_Consumer.class);
 	/**
 	 * 使用 使用restTemplate访问restful接口非常的简单粗暴无脑。 (url, requestMap,
 	 * ResponseBean.class)这三个参数分别代表 REST请求地址、请求参数、HTTP响应转换被转换成的对象类型。
@@ -48,6 +54,18 @@ public class DeptController_Consumer
 	public Object discovery()
 	{
 		return restTemplate.getForObject(REST_URL_PREFIX + "/dept/discovery", Object.class);
+	}
+	
+	@Autowired
+	private LoadBalancerClient loadBalancerClient;
+	
+	@GetMapping("log-user-instance")
+	public void ogUserInstance() {
+		ServiceInstance serviceInstance =this.loadBalancerClient.choose("eureka-client");
+		
+		// 打印当前选择的是哪个节点
+		DeptController_Consumer.LOGGER.info("{}:{}:{}", serviceInstance.getServiceId(), serviceInstance.getHost(), serviceInstance.getPort());
+
 	}
 
 }
